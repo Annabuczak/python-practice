2cc80851f92bbec02e946f6b99236b6c525604c4
Anna Buczak <ab25014@essex.ac.uk>
2026-01-21 14:16:38 +0000

    if  else

---
+++ src/ifelse.py
@@ -0,0 +1,14 @@
+age=int(input("how old are you?"))
+if age >=18:
+    print ("you are an adult")
+else:
+    print ("you are under 18")
+
+aage = int(input("How old are you? "))
+
+if age < 13:
+    print("You are a child")
+elif age >13:
+    print("You are a teenager")
+else:
+    print("You are an adult")
