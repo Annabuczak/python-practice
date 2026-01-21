cdd1ab69762e6844a8fe31baa842b409d4765225
Anna Buczak <ab25014@essex.ac.uk>
2026-01-21 13:41:05 +0000

    Variable Input

---
+++ src/basics.py
@@ -0,0 +1,15 @@
+from ssl import HAS_NEVER_CHECK_COMMON_NAME
+
+age=10
+name="anna"
+age=30
+print(name)
+print(age)
+name=input("what is your name?")
+age=input("how old are you?")
+print("Hello ", name)
+print("Your age", age, "years old")
+print(f"Hello {name}")
+print(f"you are {age} years old")
+print(f"Hello {name}")
+print(f"you are {age} years old")
