d66a9811f7240d83493dce2f9bb7429931542ffd
Anna Buczak <ab25014@essex.ac.uk>
2026-01-21 14:44:02 +0000

    if  else  while  for

---
+++ src/loop.py
@@ -0,0 +1,16 @@
+for i in range(1, 6):
+    print(i)
+for i in range(1, 6):
+    print(i)
+for i in range(3):
+    print("hi")
+for i in range(4):
+    print("annananananana")
+count = 1
+while count <=5:
+    print(count)
+    count +=1
+password = ""
+while password != "secret":
+    password = input("enter password")
+    print("access granted")
